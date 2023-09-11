19ebf8240c43ac3317e1b639f64b3653599ead67
lrickelme <rickelmelucas200906@gmail.com>
2023-09-11 16:29:32 -0300

    Atividade finalizada

---
+++ src/main/java/PontoLinha.java
@@ -0,0 +1,56 @@
+class Ponto {
+    private double x;
+    private double y;
+
+    public Ponto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setXY (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Linha {
+    private Ponto inicio;
+    private Ponto fim;
+
+    public Linha(Ponto inicio, Ponto fim) {
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+    public Ponto getInicio() {
+        return inicio;
+    }
+    public void setInicio(Ponto inicio) {
+        this.inicio = inicio;
+    }
+    public Ponto getFim() {
+        return fim;
+    }
+    public void setFim(Ponto fim) {
+        this.fim = fim;
+    }
+
+    public double getComprimento (){
+        return Math.sqrt(Math.pow(fim.getX() - inicio.getX(), 2) + Math.pow(fim.getY() - inicio.getY(), 2));
+    }
+}
